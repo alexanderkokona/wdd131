753ba714f993fc3226e286bb58c8688a87437239
alexanderkokona <alexanderkokona@gmail.com>
2025-06-03 10:29:48 -0600

    created a credit card form

---
+++ hw/creditcard/card.js
@@ -0,0 +1,40 @@
+function isCardNumberValid(number) {
+    return number === '1234123412341234';
+  }
+  
+  function displayError(msg) {
+    document.querySelector('.errorMsg').innerText = msg;
+  }
+  
+  function submitHandler(event) {
+    event.preventDefault();
+    let errorMsg = '';
+    const cardNum = this.cardNumber.value;
+    const expMonth = parseInt(this.expMonth.value, 10);
+    const expYear = parseInt(this.expYear.value, 10);
+  
+    displayError('');
+  
+    if (isNaN(cardNum)) {
+      errorMsg += 'Card number is not a valid number.\n';
+    } else if (!isCardNumberValid(cardNum)) {
+      errorMsg += 'Card number is not valid.\n';
+    }
+  
+    const now = new Date();
+    const enteredDate = new Date(expYear, expMonth - 1);
+    if (enteredDate <= now) {
+      errorMsg += 'Expiration date must be in the future.\n';
+    }
+  
+    if (errorMsg) {
+      displayError(errorMsg);
+      return false;
+    }
+  
+    alert('Form submitted successfully!');
+    return true;
+  }
+  
+  document.querySelector('#credit-card').addEventListener('submit', submitHandler);
+  
